fd9c10dcf1bc74bcf48789cf3637483daab1e3c3
andreac30 <50836459+andreac30@users.noreply.github.com>
2020-10-19 17:48:02 +0200

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  ExampleBTNNavBar WatchKit Extension
+//
+//  Created by Andrea Culot on 18/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var changed = false
+    @State var colors: [Color] = [.orange, .pink , .yellow]
+    
+    var body: some View {
+        ScrollView {
+            ScrollViewReader { value in
+                Button("Change Color") {
+                    changed.toggle()
+                    
+                    if changed {
+                        colors = [.red, .green, .blue]
+                    } else {
+                        colors = [.orange, .pink , .yellow]
+                    }
+                }
+
+                ForEach(0..<10) { i in
+                    Text("# \(i)")
+                        .frame(width: 100, height: 100)
+                        
+                        .background(colors[i % colors.count]).cornerRadius(50)
+                        .id(i)
+                        .padding()
+                        
+                }.onAppear() {
+                    value.scrollTo(0)
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
